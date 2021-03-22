32aa3f5da1aecff1231db9d38f2a26b62bf1bf2b
dev-zam <zamsyh.work@gmail.com>
2021-03-22 20:13:14 +0700

    Create page livewire Signin

---
+++ app/Http/Livewire/Auth/Signin.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Livewire\Auth;
+
+use Livewire\Component;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class Signin extends Component
+{
+
+    public $email, $password;
+    public $redirect;
+
+    public function render()
+    {
+
+        return view('livewire.auth.signin')->extends('layouts.client')->section('content');
+    }
+
+
+    protected $messages = [
+        'email.exists' => 'Email is not registered in the database',
+    ];
+
+    protected $validationAttributes = [
+        'email' => 'email address'
+    ];
+
+    public function updated($signin)
+    {
+        $this->validateOnly($signin, [
+            'email' => 'email|exists:users,email',
+            'password' => 'min:6',
+        ]);
+    }
+
+
+    public function signin()
+    {
+        $this->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+
+        try {
+
+            if (Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
+                $this->alert('success', 'Succesfully login, redirecting..', [
+                    'position' =>  'top-end',
+                    'timer' =>  3000,
+                    'toast' =>  true,
+                    'text' =>  '',
+                    'confirmButtonText' =>  'Ok',
+                    'cancelButtonText' =>  'Cancel',
+                    'showCancelButton' =>  false,
+                    'showConfirmButton' =>  false,
+                ]);
+
+
+                $this->redirect = true;
+            } else {
+                $this->alert('error', 'Oppss, invalid data', [
+                    'position' =>  'top-end',
+                    'timer' =>  3000,
+                    'toast' =>  true,
+                    'text' =>  '',
+                    'confirmButtonText' =>  'Ok',
+                    'cancelButtonText' =>  'Cancel',
+                    'showCancelButton' =>  false,
+                    'showConfirmButton' =>  false,
+                ]);
+            }
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+}
