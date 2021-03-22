235b4c49ece07b831ee8a908d2da65dfba0e350a
dev-zam <zamsyh.work@gmail.com>
2021-03-22 20:06:44 +0700

    Layouting client

---
+++ resources/views/layouts/client.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exam - @yield('title')</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-icons/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/pages/auth.css') }}">
+
+    @livewireStyles
+</head>
+
+<body>
+    @yield('content')
+
+    @livewireScripts
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+    <x-livewire-alert::scripts />
+</body>
+
+</html>
