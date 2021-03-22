a8272a770c2463f70b2326fa675da334cd52606b
dev-zam <zamsyh.work@gmail.com>
2021-03-22 20:07:27 +0700

    Create component navbar

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <footer>
+        <div class="footer clearfix mb-0 text-muted">
+            <div class="float-start">
+                <p>2021 &copy; <a href="https://github.com/zamisyh/laravel-exam">Exam</a></p>
+            </div>
+            <div class="float-end">
+                <p>Copyright by <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+                    by <a href="https://github.com/zamisyh">Zamzam</a> and Template using <a href="https://github.com/zuramai/mazer">Mazer</a></p>
+            </div>
+        </div>
+    </footer>
+</div>
