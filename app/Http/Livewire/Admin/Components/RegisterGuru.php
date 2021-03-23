236f16b0c1c199df7164c6418d5b990478d98327
dev-zam <zamsyh.work@gmail.com>
2021-03-23 20:39:52 +0700

    Create component livewire for register guru

---
+++ app/Http/Livewire/Admin/Components/RegisterGuru.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Components;
+
+use Livewire\Component;
+
+class RegisterGuru extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.components.register-guru');
+    }
+}
