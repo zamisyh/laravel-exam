2a831a780d02e2e965671f298a94a8f84623221a
dev-zam <zamsyh.work@gmail.com>
2021-03-23 20:50:42 +0700

    Update for create next page register siswa and guru

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -4,13 +4,34 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\guru;
+use App\Models\siswa;
 
 class Dashboard extends Component
 {
+
+    public $openRegisterSiswa, $openRegisterGuru;
+
+    public function mount()
+    {
+        $user = User::where('id', Auth::user()->id)->first();
+
+        if ($user->is_active == 0 && $user->getRoleNames()[0] == 'siswa') {
+            $this->openRegisterSiswa = true;
+        } else if ($user->is_active == 0 && $user->getRoleNames()[0] == 'guru') {
+            $this->openRegisterGuru = true;
+        } else {
+            //
+        }
+    }
+
+
     public function render()
     {
         return view('livewire.dashboard')->extends('layouts.app')->section('content');
     }
+
 
 
     public function logout()
