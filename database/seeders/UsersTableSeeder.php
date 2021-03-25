29df149c2d1a1c4716841e2c49a840733d56d8e5
dev-zam <zamsyh.work@gmail.com>
2021-03-25 16:37:39 +0700

    Automation seeder role

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
+use App\Models\mapel;
+use App\Models\kelas;
+use App\Models\jurusan;
 
 class UsersTableSeeder extends Seeder
 {
@@ -15,9 +18,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        Role::create([
-            'name' => 'admin'
-        ]);
+
+        $role = [
+            ['name' => 'admin', 'guard_name' => 'web'],
+            ['name' => 'guru', 'guard_name' => 'web'],
+            ['name' => 'siswa', 'guard_name' => 'web'],
+        ];
+
+        Role::insert($role);
 
         $user = User::create([
             'name' => 'zamzam',
@@ -26,5 +34,24 @@ class UsersTableSeeder extends Seeder
         ]);
 
         $user->assignRole('admin');
+
+
+        $jurusan = [
+            ['nama' => 'Rekayasa Perangkat Lunak', 'alias' => 'RPL'],
+            ['nama' => 'Perbankan Syariah', 'alias' => 'PBS'],
+            ['nama' => 'Perbankan', 'alias' => 'PB'],
+            ['nama' => 'Kimia Analis', 'alias' => 'KA'],
+            ['nama' => 'Teknik Elektronika Industri', 'alias' => 'TEI'],
+        ];
+
+        $mapel = [
+            ['nama' => 'Matematika'],
+            ['nama' => 'Pendidikan Agama Islam'],
+            ['nama' => 'Bahasa Inggris'],
+            ['nama' => 'Bahasa Indonesia'],
+        ];
+
+        mapel::insert($mapel);
+        jurusan::insert($jurusan);
     }
 }
