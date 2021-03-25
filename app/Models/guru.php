f44a75457f58df8663cc8a2db6c1403ff65317d7
dev-zam <zamsyh.work@gmail.com>
2021-03-25 16:42:27 +0700

    Create relations for handle pivot

--- app/Models/guru.php
+++ app/Models/guru.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class guru extends Model
 {
     use HasFactory;
+    protected $table = 'guru';
+
+
+    protected $fillable = ['nama', 'nip', 'jenis_kelamin', 'agama', 'alamat',  'user_id'];
+
+
+    public function kelas()
+    {
+        return $this->belongsToMany(kelas::class)->withTimestamps();
+    }
+
+    public function mapel()
+    {
+        return $this->belongsToMany(mapel::class)->withTimestamps();
+    }
 }
