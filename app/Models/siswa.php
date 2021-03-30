1fbee6a10237c13b6c27a524788f1007c7b0cd78
dev-zam <zamsyh.work@gmail.com>
2021-03-30 10:30:27 +0700

    Adding relations

--- app/Models/siswa.php
+++ app/Models/siswa.php
@@ -10,4 +10,19 @@ class siswa extends Model
     use HasFactory;
     protected $table = 'siswa';
     protected $fillable = ['nama', 'agama', 'jenis_kelamin', 'nis', 'jurusan_id', 'kelas_id', 'user_id'];
+
+    public function jurusan()
+    {
+        return $this->belongsTo(jurusan::class);
+    }
+
+    public function kelas()
+    {
+        return $this->belongsTo(kelas::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
