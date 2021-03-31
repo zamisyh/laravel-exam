a3e02c192073324368e16af99130adb3cbf1e25b
dev-zam <zamsyh.work@gmail.com>
2021-03-31 16:29:28 +0700

    Change title to 500

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>404 - System Error</title>
+        <title>500 - System Error</title>
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-icons/bootstrap-icons.css') }}">
