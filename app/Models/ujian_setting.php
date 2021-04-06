50e699fb7212f39d363979b93ec425f0a49815e9
dev-zam <zamsyh.work@gmail.com>
2021-04-06 23:54:42 +0700

    Create relation and mass assignment

--- app/Models/ujian_setting.php
+++ app/Models/ujian_setting.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class ujian_setting extends Model
 {
     use HasFactory;
+    protected $table = 'ujian_setting';
+    protected $fillable = ['ujian_id', 'jumlah_soal', 'waktu', 'urutan_soal', 'urutan_pilihan', 'tampil_nilai'];
+
+    public function ujian()
+    {
+        return $this->belongsTo(ujian::class);
+    }
 }
