4f4081d94d4519e73f37dc2b3fbf8fe92fc361ad
dev-zam <zamsyh.work@gmail.com>
2021-04-27 15:33:04 +0700

    Create relation for table hasil

--- app/Models/hasil.php
+++ app/Models/hasil.php
@@ -7,5 +7,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class hasil extends Model
 {
+
     use HasFactory;
+    protected $table = 'hasil';
+    protected $fillable = ['siswa_id', 'ujian_id', 'score', 'status'];
+
+
+    public function ujian()
+    {
+        return $this->belongsTo(ujian::class);
+    }
+
+    public function mapel()
+    {
+        return $this->belongsTo(mapel::class);
+    }
+
+    public function siswa()
+    {
+        return $this->belongsTo(siswa::class);
+    }
+
+    public function jurusan()
+    {
+        return $this->belongsTo(jurusan::class);
+    }
+
+    public function kelas()
+    {
+        return $this->belongsTo(kelas::class);
+    }
+
+
+    public function ujian_setting()
+    {
+        return $this->hasMany(ujian_setting::class);
+    }
 }
