ee90a1a56ded4d32241aa345e0c558eb9ab999e9
dev-zam <zamsyh.work@gmail.com>
2021-04-27 15:31:20 +0700

    Edited

--- database/migrations/2021_03_20_134707_create_hasil_table.php
+++ database/migrations/2021_03_20_134707_create_hasil_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateHasilsTable extends Migration
+class CreateHasilTable extends Migration
 {
     /**
      * Run the migrations.
@@ -17,13 +17,12 @@ class CreateHasilsTable extends Migration
             $table->id();
             $table->unsignedBigInteger('siswa_id');
             $table->unsignedBigInteger('ujian_id');
-            $table->unsignedBigInteger('score');
-
+            $table->integer('score')->nullable();
+            $table->boolean('status')->default(false);
+            $table->timestamps();
 
             $table->foreign('siswa_id')->references('id')->on('siswa')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('ujian_id')->references('id')->on('ujian')->onDelete('cascade')->onUpdate('cascade');
-
-            $table->timestamps();
         });
     }
 
@@ -34,6 +33,6 @@ class CreateHasilsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('hasils');
+        Schema::dropIfExists('hasil');
     }
 }
