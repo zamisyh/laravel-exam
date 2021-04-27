4656a644734f6a6d720b840f6571d32b95f2f3fe
dev-zam <zamsyh.work@gmail.com>
2021-04-27 15:33:42 +0700

    Updating and add new function for dashboard

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -7,9 +7,10 @@ use Livewire\WithPagination;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\guru;
-use App\Models\mapel;
 use App\Models\siswa;
 use App\Models\ujian;
+use App\Models\hasil;
+use Illuminate\Support\Facades\Session;
 
 class Dashboard extends Component
 {
@@ -20,6 +21,7 @@ class Dashboard extends Component
     public $openRegisterSiswa, $openRegisterGuru;
 
     public $perPage = 3;
+    public $amount = 2;
 
     public function mount()
     {
@@ -41,15 +43,44 @@ class Dashboard extends Component
         $data = null;
 
         if ($user->getRoleNames()[0] == 'guru') {
-            $data['data']['countSiswa'] = siswa::count();
             $guru = guru::where('user_id', Auth::user()->id)->first();
-            $data['data']['countUjian'] = ujian::where('guru_id', $guru->id)->count();
-            $data['data']['ujian'] = ujian::with('ujian_setting', 'guru', 'mapel', 'kelas.jurusan')->orderBy('created_at', 'DESC')->paginate($this->perPage);
+            $data['data']['null'] = null;
+            if ($user->is_active == 1 && $user->getRoleNames()[0] == 'guru') {
+                $kelas = [];
+                for ($i = 0; $i < count($guru->kelas); $i++) {
+                    $kelas[] = $guru->kelas[$i]->id;
+                }
 
+                $data['data']['activity'] = hasil::with('siswa', 'ujian', 'ujian.mapel', 'kelas')
+                    ->orderBy('created_at', 'DESC')->take($this->amount)->get();
+
+
+                $data['data']['countSiswa'] = siswa::where('kelas_id', $kelas)->count();
+                $data['data']['countUjian'] = ujian::where('guru_id', $guru->id)->count();
+                $data['data']['ujian'] = ujian::where('guru_id', $guru->id)->with('ujian_setting', 'guru', 'mapel', 'kelas.jurusan')->orderBy('created_at', 'DESC')->paginate($this->perPage);
+            }
+        } elseif ($user->getRoleNames()[0] == 'siswa') {
+            $data['data']['siswa'] = siswa::where('user_id', $user->id)->first();
+            $data['data']['null'] = null;
+            if ($user->is_active == 1 && $user->getRoleNames()[0] == 'siswa') {
+                $data['data']['ujian'] = ujian::where('status', 1)
+                    ->where('kelas_id', $data['data']['siswa']['kelas_id'])
+                    ->with('ujian_setting', 'soal', 'guru', 'mapel')
+                    ->orderBy('created_at', 'DESC')
+                    ->get();
+                $data['data']['hasil'] = hasil::where('siswa_id', $data['data']['siswa']['id'])
+                    ->with('ujian.ujian_setting', 'ujian.mapel')->get();
+            }
         } else {
             $data['data']['null'] = null;
         }
 
         return view('livewire.dashboard', $data)->extends('layouts.app')->section('content');
+    }
+
+
+    public function viewMore()
+    {
+        $this->amount += 2;
     }
 }
